d5ec74ec902ddfa7b9796e37dcf5f67ba1f6889a
PROMLEE <donghoon099@naver.com>
2024-08-10 15:36:24 +0900

    Feat: 배송지 입력 body 에러 처리

--- src/main/java/com/prototyne/web/controller/DeliveryController.java
+++ src/main/java/com/prototyne/web/controller/DeliveryController.java
@@ -7,6 +7,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
@@ -35,7 +36,7 @@ public class DeliveryController {
     @Operation(summary = "배송지 수정 API - 인증 필요",
             description = "유저 배송지 수정",
             security = {@SecurityRequirement(name = "session-token")})
-    public ApiResponse<DeliveryDto> PatchDelivery(HttpServletRequest token, @RequestBody DeliveryDto newDeliveryDto) {
+    public ApiResponse<DeliveryDto> PatchDelivery(HttpServletRequest token, @RequestBody @Valid DeliveryDto newDeliveryDto) {
         String aouthtoken = token.getHeader("Authorization").replace("Bearer ", "");
         DeliveryDto deliveryDto = deliveryService.patchDeliveryInfo(aouthtoken, newDeliveryDto);
         return ApiResponse.onSuccess(deliveryDto);
