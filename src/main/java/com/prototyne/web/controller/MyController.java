0a5127b1ddee683a393f738753799d066f981b9d
PROMLEE <donghoon099@naver.com>
2024-08-11 17:32:20 +0900

    Feat: Swagger description 추가

--- src/main/java/com/prototyne/web/controller/MyController.java
+++ src/main/java/com/prototyne/web/controller/MyController.java
@@ -18,45 +18,57 @@ import java.nio.file.attribute.UserPrincipalNotFoundException;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/my")
-@Tag(name = "02. 내 정보 - 기본", description = "유저 정보 관련 API")
+@Tag(name = "${swagger.tag.my}")
 public class MyController {
 
     private final UserDetailServiceImpl userDetailService;
 
-    @Tag(name = "${swagger.tag.my}")
     @GetMapping("/detail")
     @Operation(summary = "유저 정보 조회 API - 인증 필요",
             description = "유저 정보 조회 API - 인증 필요",
             security = {@SecurityRequirement(name = "session-token")})
     public ApiResponse<UserDto.UserDetailResponse> getUserDetail(HttpServletRequest request) throws Exception {
         String accessToken = request.getHeader("Authorization").replace("Bearer ", "");
-        log.info("JWT Token: {}", accessToken);
         UserDto.UserDetailResponse userDetailResponse = userDetailService.getUserDetail(accessToken);
         return ApiResponse.onSuccess(userDetailResponse);
     }
 
     @PatchMapping("/basicInfo")
     @Operation(summary = "필수 정보 수정 API - 인증 필요",
-            description = "유저의 필수 정보를 수정합니다.",
+            description = """
+                    유저의 필수 정보를 수정합니다.
+                   \s
+                    [body] \s
+                    *familymember: 가족 구성원 수(int), \s
+                    *gender: "MALE" | "FEMALE", \s
+                    *birth: "YYYY-MM-DD"
+                   \s""",
             security = {@SecurityRequirement(name = "session-token")})
-    public ApiResponse<UserDto.DetailInfo> updateDetailInfo(HttpServletRequest request,
-                                                @RequestBody @Valid UserDto.DetailInfo detailInfo) throws UserPrincipalNotFoundException {
+    public ApiResponse<UserDto.DetailInfo> updateDetailInfo(HttpServletRequest request, @RequestBody @Valid UserDto.DetailInfo detailInfo) throws UserPrincipalNotFoundException {
         String accessToken = request.getHeader("Authorization").replace("Bearer ", "");
-        log.info("JWT Token: {}", accessToken);
-
         return ApiResponse.onSuccess(userDetailService.updateBasicInfo(accessToken, detailInfo));
 
     }
 
     @PatchMapping("/addInfo")
     @Operation(summary = "추가 정보 수정 API - 인증 필요",
-            description = "유저의 추가 정보를 수정합니다.",
+            description = """
+                    유저의 추가 정보를 수정합니다. - 수정 필요한 key-value 만 입력
+                    \s
+                    [body] \s
+                    occupation: 직업(string) - | "STUDENT" | "OFFICE" | "PROFESSIONAL" | "SELFEMPLOYED" | "OTHER", \s
+                    income: 수입(int) - | 2000 | 4000 | 6000 | 8000 | 9999, \s
+                    interests: 관심사(string[]) - | 1 | "COUPLE" | "COUPLE&CHILDREN" | "PARENTS&CHILDREN" | "EXTENDFAMILY", \s
+                    familyComposition: 가족 구성(string), - | 1 | "COUPLE" | "COUPLE&CHILDREN" | "PARENTS&CHILDREN" | "EXTENDFAMILY" \s
+                    productTypes: 관심 상품(string[]) - | "ELECTRONIC" | "FASHION&BEAUTY" | "FOOD" | "HOUSEHOLD" | "HEALTH", \s
+                    phones: 휴대폰 종류(string[]) - | "SMARTPHONE1" | "SMARTPHONE2" | "SMARTPHONE9" | "TABLET" | "SMARTWATCH", \s
+                    healthStatus: 건강 상태(int) - | 1 | 2 | 3 | 4 | 5
+                    \s
+                   \s""",
             security = {@SecurityRequirement(name = "session-token")})
     public ApiResponse<UserDto.AddInfo> updateAddInfo(HttpServletRequest request,
                                          @RequestBody @Valid UserDto.AddInfo addInfo) throws UserPrincipalNotFoundException {
         String accessToken = request.getHeader("Authorization").replace("Bearer ", "");
-        log.info("JWT Token: {}", accessToken);
-
         return ApiResponse.onSuccess(userDetailService.updateAddInfo(accessToken, addInfo));
     }
 
