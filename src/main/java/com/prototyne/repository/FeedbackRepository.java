7ff692cf156cdd2a60b0bfbcec3d889aaeea7407
gaeul3303 <rkdmfghd@naver.com>
2024-08-16 01:00:19 +0900

    Fix: FeedbackRepository 충돌 수정

--- src/main/java/com/prototyne/repository/FeedbackRepository.java
+++ src/main/java/com/prototyne/repository/FeedbackRepository.java
@@ -1,13 +1,14 @@
 package com.prototyne.repository;
 
 import com.prototyne.domain.Feedback;
-import com.prototyne.domain.Investment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface FeedbackRepository extends JpaRepository<Feedback, Long> {
+    List<Feedback> findByUserId(Long userId);
     Optional<Feedback> findByInvestmentId(Long investmentId);
 }
