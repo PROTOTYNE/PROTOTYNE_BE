e2f4ec9b9f0191808e9484b609b3f0665ab848b3
PROMLEE <donghoon099@naver.com>
2024-08-19 22:55:39 +0900

    bug: PATCH 메소드 추가, 허용 도메인 설정

--- src/main/java/com/prototyne/apiPayload/config/WebConfig.java
+++ src/main/java/com/prototyne/apiPayload/config/WebConfig.java
@@ -5,13 +5,13 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig  implements WebMvcConfigurer {
+public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedOriginPatterns("https://prototyne.netlify.app", "http://localhost:5173")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
