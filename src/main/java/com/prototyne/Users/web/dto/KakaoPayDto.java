2fc8309f48472eaaeaf8df4743eb4e2899f47ef4
베뉴 <christer10@ewhain.net>
2024-09-17 04:02:30 +0900

    feat: 카카오페이 결제 준비 로직 구현

---
+++ src/main/java/com/prototyne/Users/web/dto/KakaoPayDto.java
@@ -0,0 +1,53 @@
+package com.prototyne.Users.web.dto;
+
+import com.prototyne.domain.enums.TicketOption;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+
+import java.util.UUID;
+
+public class KakaoPayDto {
+
+    @Getter
+    @Setter
+    public static class KakaoPayReadyResponse{
+        private String tid;
+        private String next_redirect_pc_url;
+        private String created_at;
+    }
+
+    @Data
+    public static class KakaoPayRequest {
+        private String orderId; // 결제 건 고유 번호
+        private Long userId;
+        private String itemName;
+        private int quantity;
+        private int totalAmount;
+
+        public KakaoPayRequest(Long userId, TicketOption ticketOption){
+            this.orderId = UUID.randomUUID().toString();
+            this.userId = userId;
+            this.itemName = ticketOption.getTicketNumber() + " 티켓 구매";
+            this.quantity = 1;
+            this.totalAmount = ticketOption.getPrice();
+        }
+
+        public MultiValueMap<String, String> toMutliValueMap() {
+            MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
+            map.add("cid", "TC0ONETIME");  // 테스트용 CID
+            map.add("partner_order_id", orderId);
+            map.add("partner_user_id", userId.toString());
+            map.add("item_name", itemName);
+            map.add("quantity", String.valueOf(quantity));
+            map.add("total_amount", String.valueOf(totalAmount));
+            map.add("tax_free_amount", "0");
+            map.add("approval_url", "http://localhost:8080/payment/success");
+            map.add("cancel_url", "http://localhost:8080/payment/cancel");
+            map.add("fail_url", "http://localhost:8080/payment/fail");
+            return map;
+        }
+    }
+}
