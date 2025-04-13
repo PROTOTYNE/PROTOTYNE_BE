1650c2d8021dda6c29ffe68d647831d77e05d813
베뉴 <christer10@ewhain.net>
2025-04-14 01:55:51 +0900

    component scan route update

--- src/main/java/com/prototyne/StudyApplication.java
+++ src/main/java/com/prototyne/StudyApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.prototyne")
 @EnableJpaAuditing
 @EnableScheduling
 public class StudyApplication {
